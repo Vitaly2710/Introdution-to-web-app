7d92f15e64eeee94b36e2f8cd6a523d556133434
Vitaly <cheastyakov3@gmail.com>
2022-03-17 17:03:25 +0300

    added Extra-task from js intro second task

--- Extra tasks/Extra-task 2/js.js
+++ Extra tasks/Extra-task 2/js.js
@@ -10,9 +10,40 @@ function maxSum (arr) {
     })
     return sum
 }
+//test maxSum
+// console.log(maxSum([1,2,56,-1,13,12,-87,23]));
+// console.log(maxSum([-2,1,-3,4,-1,2,1,-5,4]));
 
-console.log(maxSum([1,2,56,-1,13,12,-87,23]));
-console.log(maxSum([-2,1,-3,4,-1,2,1,-5,4]));
+
+function brokerProfit (arr) {
+    let pointOfPurchase = 0;
+    let buySell = 0;
+    arr.forEach((elem,indexElem) => {
+
+        arr.forEach((item,indexItem) => {
+
+            if(indexElem >= indexItem){
+                return
+            } else {
+                if(elem > item){
+                    buySell = -(elem - item)
+                } else {
+                    buySell = item - elem
+                    pointOfPurchase = Math.max(buySell,pointOfPurchase)
+                }
+            }
+
+
+        })
+    })
+    return pointOfPurchase
+}
+
+//test brokerProfit
+// console.log(brokerProfit([7,1,5,4,6,4]))
+// console.log(brokerProfit([1,2,3,4,5]))
+// console.log(brokerProfit([7,6,4,3,1]))
+
 
 
 
